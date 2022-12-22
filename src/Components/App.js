552e986052247067efc436a53d91f0dbcad4c9fd
Tron Nguyen <33529067+pnguyen46@users.noreply.github.com>
2022-12-22 12:16:37 -0500

    update apikey

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,40 +1,39 @@
-import React,{useState,useEffect} from 'react'
-import '../App.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import axios from "axios";
 
-import SearchForm from './SearchForm';
-import GifList from './GifList';
+import SearchForm from "./SearchForm";
+import GifList from "./GifList";
 
 const App = () => {
-  const apiKey = 'DqiiWZTO4ewfbx2OBPgF1DjiIDCPLQ1D';
-  const [data,setData] = useState([]);
-  const [query,setQuery] = useState('cats');
-  const [isLoading,setIsLoading] = useState(true);
+  const apiKey = "DqiiWZTO4ewfbx2OBPgF1DjiIDCPLQ1D";
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("puppy");
+  const [isLoading, setIsLoading] = useState(true);
 
   //retrieve data from server using the "query" state
   useEffect(() => {
     axios(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${apiKey}`)
-      .then(res => setData(shuffleArray(res.data.data)))
-      .catch(error => console.log(`Error fetching and parsing data`,error))
-      .finally(() => setIsLoading(false))
-  },[query])
+      .then((res) => setData(shuffleArray(res.data.data)))
+      .catch((error) => console.log(`Error fetching and parsing data`, error))
+      .finally(() => setIsLoading(false));
+  }, [query]);
 
   //Set the query value
   const performSearch = (value) => {
     setQuery(value);
-  }
+  };
 
   /* Randomize array */
   const shuffleArray = (array) => {
     for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-      return array;
-  }
-
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
 
   return (
     <>
@@ -47,11 +46,10 @@ const App = () => {
       </div>
       <div className="main-content">
         {/* pass down the data state */}
-        {isLoading ? <p>Loading...</p> : <GifList data={data}/>}
+        {isLoading ? <p>Loading...</p> : <GifList data={data} />}
       </div>
     </>
   );
-}
+};
 
-export default App
-
+export default App;
